97439b8079ed235d7b7e71f74c936e02fc68e434
jesuscolmenaresp <colmenaresjesus94@gmail.com>
2025-05-26 20:17:07 -0400

    Reemplazado bcrypt por bcryptjs para compatibilidad con Render

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const dbUsers = require('../models/users');
 
 // Página de registro
